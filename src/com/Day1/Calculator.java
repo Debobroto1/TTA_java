7502817ca6c47c5bb16c820182d77ce5f05b6b2c
debo <devaryan.sen@gmail.com>
2022-12-06 01:08:12 +0530

    Modified the calculator program

--- src/com/Day1/Calculator.java
+++ src/com/Day1/Calculator.java
@@ -12,16 +12,16 @@ public class Calculator {
         System.out.println("Enter second number");
         int secondNum=sc.nextInt();
         switch(operator) {
-            case 1:
+            case '+':
                 performAddition(firstNum,secondNum);
                 break;
-            case 2:
+            case '-':
                 performSubstraction(firstNum,secondNum);
                 break;
-            case 3:
+            case '/':
                 performDivision(firstNum,secondNum);
                 break;
-            case 4:
+            case '*':
                 performMultiplication(firstNum,secondNum);
                 break;
             default:
@@ -32,22 +32,23 @@ public class Calculator {
     //Method for addition
     public static void performAddition ( int num1, int num2){
         int result = num1 + num2;
-        System.out.println(result);
+        System.out.println("The sum of "+num1+" and "+num2+" = "+result);
     }
     //Method for subtraction
     public static void performSubstraction ( int num1, int num2){
         int result = num1 - num2;
-        System.out.println(result);
+        System.out.println("The difference of "+num1+" and "+num2+" = "+result);
     }
     //Method for multiplication
     public static void performMultiplication ( int num1, int num2){
         int result = num1 * num2;
-        System.out.println(result);
+        System.out.println("The product of "+num1+" and "+num2+" = "+result);
     }
     //Method for division
     public static void performDivision ( int num1, int num2){
         try {
             int result = num1 / num2;
+            System.out.println("The quotient of the "+num1+" and "+num2+" = "+result);
         }
         catch (ArithmeticException e){
             System.out.println("You should not divide a number by zero");
